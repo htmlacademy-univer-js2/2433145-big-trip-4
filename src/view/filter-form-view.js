4a96f96980dbf75d03f558b78ca361642b049ee8
Emma-Shifter <n.ba.04@mail.ru>
2024-03-12 22:55:21 +0500

    check

--- src/view/filter-form-view.js
+++ src/view/filter-form-view.js
@@ -1,15 +1,7 @@
-import { createElement } from '../render.js';
 import { createFilterFormTemplate } from '../templates/filter-form-template.js';
 
 export default class FilterFormView {
   getTemplate() {
     return createFilterFormTemplate();
-  }
-
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
-    }
-    return this.element;
   }
 }
